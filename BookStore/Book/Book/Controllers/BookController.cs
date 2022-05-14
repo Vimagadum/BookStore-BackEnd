eaf25ae8b8a551ee128d093b2b842eb09be694f0
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-05-14 16:58:28 +0530

    [Vinayak] added API for delete book

--- BookStore/Book/Book/Controllers/BookController.cs
+++ BookStore/Book/Book/Controllers/BookController.cs
@@ -78,6 +78,32 @@ namespace Book.Controllers
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
-        
+        [Route("delete")]
+        [HttpDelete]
+        public HttpResponseMessage DeleteBook(int id)
+        {
+            try
+            {
+                var book = session.Get<BookModel>(id);
+                if (book != null)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Delete(book);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, "Book deleted Successfully");
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }
