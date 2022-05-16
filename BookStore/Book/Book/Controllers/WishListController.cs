1273e1b961628a6ec340243bd77f1de211dc9ebc
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-05-16 14:52:41 +0530

    [Vinayak] added API for add to wish list

---
+++ BookStore/Book/Book/Controllers/WishListController.cs
@@ -0,0 +1,44 @@
+﻿using Book.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Book.Controllers
+{
+    public class WishListController : ApiController
+    {
+        ISession session = OpenSessionsss.OpenSession();
+        //Get all Notes
+
+
+        [Route("AddWishList")]
+        [HttpPost]
+        public HttpResponseMessage AddAddress(WishlistModel wish)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(wish);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, wish);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
---
+++ BookStore/Book/Book/Models/WishlistModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book.Models
+{
+    public class WishlistModel
+    {
+        public virtual int WishlistId { get; set; }
+        public virtual int UserId { get; set; }
+        [JsonIgnore]
+        public virtual UserModel UserModels { get; set; }
+        public virtual int bookId { get; set; }
+        [JsonIgnore]
+        public virtual BookModel BookModels { get; set; }
+    }
+}
