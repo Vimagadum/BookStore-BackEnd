1ccc67ab9d24bda84360ed3bf9631bf802191c54
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-05-16 15:23:45 +0530

    [Vinayak] added API for order

---
+++ BookStore/Book/Book/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using Book.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Book.Controllers
+{
+    public class OrderController : ApiController
+    {
+        ISession session = OpenSessionsss.OpenSession();
+        //Get all Notes
+
+
+        [Route("order")]
+        [HttpPost]
+        public HttpResponseMessage Order(OrderModel order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(order);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, order);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
