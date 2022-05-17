6c1e56f74cab58be300c5b21f41dc53c88d849b8
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-05-17 09:49:17 +0530

    [Vinayak] added API for update feedback

--- BookStore/Book/Book/Controllers/feedbackController.cs
+++ BookStore/Book/Book/Controllers/feedbackController.cs
@@ -40,6 +40,37 @@ namespace Book.Controllers
                 return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
-        
+        [HttpPut]
+        public HttpResponseMessage UpdateBook(int FeedbackId, feedbackModel feed)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var feedback = session.Get<feedbackModel>(FeedbackId);
+                    feedback.Comment = feed.Comment;
+                    feedback.Rating = feed.Rating;
+                    feedback.UserId = feed.UserId;
+                    feedback.BookId = feed.BookId;
+
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        session.Save(feedback);
+                        transaction.Commit();
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, feedback);
+
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error !");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }
